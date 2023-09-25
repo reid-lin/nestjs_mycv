29d6b8ee5f88f12f33da5c6868849be8259f33d2
Reid <reid.lin@nextlink.com.tw>
2023-09-25 11:53:45 +0800

    72 - implementing signup functionality

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -1,7 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, BadRequestException } from '@nestjs/common';
 import { UsersService } from './users.service';
 
 @Injectable()
 export class AuthService {
   constructor(private userService: UsersService) {}
+
+  async signup(email: string, password: string) {
+    // find user -> userService -> async, await -> user.length
+    const user = await this.userService.find(email);
+
+    if (user.length) {
+      throw new BadRequestException('email in use');
+    }
+    // hash password
+    // create user
+    // return user
+  }
+
+  signin() {}
 }
