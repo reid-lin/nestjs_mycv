9ff6aeaa196bc5244236844038b49b8085ec60ed
Reid <reid.lin@nextlink.com.tw>
2023-09-25 14:27:31 +0800

    75 - creating a user

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -1,10 +1,8 @@
 import { Injectable, BadRequestException } from '@nestjs/common';
 import { UsersService } from './users.service';
-// avoid to use callback in scrypt function
 import { randomBytes, scrypt as _scrypt } from 'crypto';
 import { promisify } from 'util';
 
-// setup promise object ot scrypt
 const scrypt = promisify(_scrypt);
 
 @Injectable()
@@ -12,20 +10,19 @@ export class AuthService {
   constructor(private userService: UsersService) {}
 
   async signup(email: string, password: string) {
-    // find user -> userService -> async, await -> user.length
-    const user = await this.userService.find(email);
+    const users = await this.userService.find(email);
 
-    if (user.length) {
+    if (users.length) {
       throw new BadRequestException('email in use');
     }
-    // hash password
     const salt = randomBytes(8).toString('hex');
-    // let script return value as Buffer for typescript know what type it is
     const hash = (await scrypt(password, salt, 32)) as Buffer;
     const result = salt + '.' + hash.toString('hex');
-
     // create user
+    const user = await this.userService.create(email, result);
+
     // return user
+    return user;
   }
 
   signin() {}
